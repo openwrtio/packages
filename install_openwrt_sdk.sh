66d7a6adb241f0a1412ce4860510050157632551
sinkcup <sinkcup@163.com>
2015-05-21 22:28:58 +0800

    add daocloud CI. resolved #1

---
+++ install_openwrt_sdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ev
+
+mkdir -p ~/Downloads/
+if [ ! -f ~/Downloads/hc5761-20140619.tar.gz ]; then
+    wget -O ~/Downloads/hc5761-20140619.tar.gz --user='yangzhou' --password='YZPassword01!' ftp://52.68.198.48/hc5761-20140619.tar.gz
+fi
+md5=`md5sum ~/Downloads/hc5761-20140619.tar.gz`
+if [ $md5 != "3bf4536657de61551d11f80fa8e804f4" ]; then
+    rm ~/Downloads/hc5761-20140619.tar.gz
+    return 1
+fi
+
+mkdir -p ~/openwrt/
+rm -rf ~/openwrt/hc5761
+tar -zxvf ~/Downloads/hc5761-20140619.tar.gz -C ~/openwrt/
