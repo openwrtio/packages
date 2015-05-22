82949d614eefd0ec111bdc3ee3abd9e6cce5609b
sinkcup <sinkcup@163.com>
2015-05-22 23:41:45 +0800

    fixbug: script: line 2: This: command not found

--- install_openwrt_sdk.sh
+++ install_openwrt_sdk.sh
@@ -5,7 +5,9 @@ mkdir -p ~/Downloads/
 if [ ! -f ~/Downloads/hc5761-20140619.tar.gz ]; then
     wget -O ~/Downloads/hc5761-20140619.tar.gz --user='yangzhou' --password='YZPassword01!' ftp://52.68.198.48/hc5761-20140619.tar.gz
 fi
+md5sum ~/Downloads/hc5761-20140619.tar.gz
 md5=`md5sum ~/Downloads/hc5761-20140619.tar.gz`
+echo $md5
 if [ $md5 != "3bf4536657de61551d11f80fa8e804f4" ]; then
     rm ~/Downloads/hc5761-20140619.tar.gz
     return 1
